02b7d93fab2d97d303a06766fddc88a63e325aca
Charles <c_onyemem@live.com>
2022-09-08 14:22:34 -0700

    commit tree a351e5967a1f47ef8a24be096dc93ad6262c8bfa

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,16 @@
 # Write your code here
+require 'pry'
+
+def 
+    reverse_each_word(sentence)
+    words = sentence.split(" ")
+    reversed_words = []
+
+    words.each do |word|
+    newArr = reverse_words.push(word)
+    binding.pry
+    end
+
+end
+
+reverse_each_word("Hello there, and how are you?")
